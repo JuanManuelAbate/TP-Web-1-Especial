0c6c2a4902acb91da0cf2a6b0c51499a4e4e35ec
Juan Manuel Abate <juan.abate@gmail.com>
2016-09-15 22:19:09 -0300

    Creado modelo categoria

---
+++ models/CategoriaModel.php
@@ -0,0 +1,39 @@
+<?php
+
+class CategoriaModel {
+
+  private $db;
+
+  function __construct() {
+
+    $this->db = new PDO('mysql:host=localhost;dbname=tpespecialdb;charset=utf8', 'root', '');
+  }
+
+  function getCategorias() {
+
+    $query = $this->db->prepare("select * from categoria");
+    $query->execute();
+    return $query->fetchAll(PDO::FETCH_ASSOC);
+  }
+
+  function createCategoria($nombreCategoria) {
+
+    $query = $this->db->prepare("insert into categoria(nombre) values(?)");
+    $query->execute(array($nombreCategoria));
+  }
+
+  function deleteCategoria($idCategoria) {
+
+    $query = $this->db->prepare("delete from categoria where id_categoria=?");
+    $query->execute(array($idCategoria));
+  }
+
+  function updateCategoria($idCategoria, $nombreCategoria) {
+
+    $query = $this->db->prepare("update categoria set nombre=? where id_categoria=?");
+    $query->execute(array($nombreCategoria, $idCategoria));
+  }
+
+}
+
+?>
